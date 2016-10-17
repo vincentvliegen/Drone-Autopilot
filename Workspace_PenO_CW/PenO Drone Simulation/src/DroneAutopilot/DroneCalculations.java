62c247eb9545605d1e2804770e93bfbe8624e814
vincentvliegen1 <vincent.vliegen@student.kuleuven.be>
2016-10-17 16:21:47 +0200

    correcties en constructor

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/DroneCalculations.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/DroneCalculations.java
@@ -1,18 +1,29 @@
 package DroneAutopilot;
 
 import p_en_o_cw_2016.*;
+import p_en_o_cw_2016.wireprotocol.TestbedStub;
 
 
 public class DroneCalculations {
-
+	
+	protected DroneCalculations(Drone drone){
+		this.setDrone(drone);
+	}
+	
 	public void depthcalculation(){
-		int camerafstand = TestbedStub.getCameraSeparation();
+		int cameraDistance = TestbedStub.getCameraSeparation();
 		
 	}
 
+private void setDrone(Drone drone){
+	this.drone = drone;
+}
 
+public Drone getDrone(){
+	return this.drone;
+}
 
-
+private Drone drone;
 
 
 
@@ -23,7 +34,8 @@ public class DroneCalculations {
 //TODO
 //algoritme uitwaartse spiraal met beginpositie
 //zoekt een rode pixel in de buurt van de verwachtte positie
-	public int getNearestPosRedPix(int expectedPos, int[] image){
+	public int getNearestPosRedPix(int expectedPos, Camera camera){
+		Camera camera = camera; 
 		int width =  getWidth();
 		int currentPos = expectedPos;
 		int columnCounter = 1;
