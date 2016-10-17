7403328332671b3415b94a8ee830930a6ce18e85
vincentvliegen1 <vincent.vliegen@student.kuleuven.be>
2016-10-17 15:57:54 +0200

    begin spiral

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/DroneCalculations.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/DroneCalculations.java
@@ -1,5 +1,23 @@
 package DroneAutopilot;
 
+import p_en_o_cw_2016.*;
+
 public class DroneCalculations {
 
+}
+
+//TODO deftige documentatie, Engels
+//TODO enum red pixel/ andere manier van aanduiden, nu als int
+//TODO
+//algoritme uitwaartse spiraal met beginpositie
+//zoekt een rode pixel in de buurt van de verwachtte positie
+	public int getNearestPosRedPix(int expectedPos, int[] image){
+		int width =  getWidth();
+		int currentPos = expectedPos;
+		int columnCounter = 1;
+		int rowCounter = 1;
+		int height = image.length/width;
+		
+
+	}
 }
