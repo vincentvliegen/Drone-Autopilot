33e68ae6e3272e31b732c076694418a5b1dd4660
Laura <lauravranken96@gmail.com>
2016-10-19 15:18:53 +0200

    commentaar methodes van github hierbij geplakt

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/Autopilot.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/Autopilot.java
@@ -2,6 +2,9 @@ package DroneAutopilot;
 
 public class Autopilot implements p_en_o_cw_2016.Autopilot{
 
+	/** Called by the testbed in the AWT/Swing GUI thread at a high (but possibly variable)
+    frequency, after simulated time has advanced and the simulated world has been
+    updated to a new state. Simulated time is frozen for the duration of this call. */
 	@Override
 	public void timeHasPassed() {
 		// TODO Auto-generated method stub
