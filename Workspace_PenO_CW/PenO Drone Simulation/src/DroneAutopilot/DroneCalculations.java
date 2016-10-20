429da4ab8232400dbb3b379902ad74eb839daa38
vincentvliegen1 <vincent.vliegen@student.kuleuven.be>
2016-10-20 16:10:33 +0200

    beetje meer

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/DroneCalculations.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/DroneCalculations.java
@@ -2,7 +2,8 @@ package DroneAutopilot;
 
 import p_en_o_cw_2016.*;
 import p_en_o_cw_2016.wireprotocol.TestbedStub;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class DroneCalculations {
 	
@@ -65,23 +66,64 @@ public class DroneCalculations {
 	
 	
 	
+	//TODO hoe worden pixels gegeven? store als int
+	private int white;
+	
+	public int[] indexToCoordinates(int index){
+		int x = (int) (index / leftCameraWidth);
+		int y = (int) (index % leftCameraWidth);
+		int[] coord = {x,y};
+	    return coord;
+	}
+	
+	public int coordinatesToIndex(int[] coordinates){
+		int index = (int) (coordinates[0]+coordinates[1]*leftCameraWidth);
+	    return index;
+	}
 
 
-
-
-//TODO deftige documentatie, Engels
-//TODO enum red pixel/ andere manier van aanduiden, nu als int
-//TODO
-//algoritme uitwaartse spiraal met beginpositie
-//zoekt een rode pixel in de buurt van de verwachtte positie
-	public int getNearestPosRedPix(int expectedPos, Camera camera){
-		Camera camera = camera; 
-		int width =  getWidth();
-		int currentPos = expectedPos;
+	//TODO
+	//zwaartepunt van groepje pixels bepalen
+	public int[] getCenterOfGravity (int[][] listOfPixelCoordinates){
+		return null;
+		
+	}
+	
+	//TODO 
+	//algoritme dat heel de afbeelding afgaat en alle zwaartepunten/alle pixels van alle kleuren stockeert
+	//dit werkt niet voor kleurenovergangen
+	private void getAllPosColPixels(Camera camera){
+		
+	}
+	
+	//alle pixels of zwtp?
+	private List<int[]> allColoredPixelsLeft = new ArrayList<int[]>();
+	private List<int[]> allColoredPixelsRight = new ArrayList<int[]>();
+	
+	
+	//TODO
+	//algoritme uitwaartse spiraal met beginpositie
+	//zoekt alle pixels af tot de gekleurde bol helemaal omvat wordt
+	//zoekt in de buurt van de verwachtte positie
+    public int[] getPosColoredOrb(int[] expectedPos, Camera camera, int colorOrbToLookFor){
+        Camera currentCamera = camera;
+        int[] currentPos = expectedPos;
+        int[] image = currentCamera.takeImage();
+        int width =  currentCamera.getWidth();
 		int columnCounter = 1;
 		int rowCounter = 1;
 		int height = image.length/width;
-		
-
+        int orbColor = colorOrbToLookFor;
+        int currentColor;
+        List<int[]> colorList = new ArrayList<int[]>();
+        for(int i = 0; image.length > i; i++){
+            currentColor = image[coordinatesToIndex(currentPos)];
+            if(currentColor == colorOrbToLookFor){//werkt niet voor kleurenovergangen
+                colorList
+            }
+        }
+        return null;		
 	}
+    
+    //TODO calculate expected pos (adhv autopilot?)
 }
