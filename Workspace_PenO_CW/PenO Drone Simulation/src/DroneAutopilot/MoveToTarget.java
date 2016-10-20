27b6fc4a8d8f586afd0c7a5f1b383dcf2c61320d
Laura Vranken <lauravranken96@gmail.com>
2016-10-20 17:25:22 +0200

    vroege push om mergeproblemem te voorkomen

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/MoveToTarget.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/MoveToTarget.java
@@ -1,11 +1,39 @@
 package DroneAutopilot;
 
+import java.util.ArrayList;
+
 public class MoveToTarget {
 
 	public MoveToTarget(){
 		
 	}
 	
+	public void checkcasespixelsfound(ArrayList leftcamera, ArrayList rightcamera){
+		if (leftcamera.isEmpty() && rightcamera.isEmpty())
+			noTargetFound();
+		else if (!leftcamera.isEmpty() && rightcamera.isEmpty())
+			leftCameraFoundTarget();
+		else if (leftcamera.isEmpty() && !rightcamera.isEmpty())
+			rightCameraFoundTarget();
+		else if (!leftcamera.isEmpty() && !rightcamera.isEmpty())
+			targetVisible();
+	}
+	
+	public void noTargetFound(){
+		
+	}
+	
+	public void leftCameraFoundTarget(){
+		
+	}
+	
+	public void rightCameraFoundTarget(){
+		
+	}
+	
+	public void targetVisible(){
+		
+	}
 	
 	public void setPitchRate(float value){
 		this.pitchRate = value;
