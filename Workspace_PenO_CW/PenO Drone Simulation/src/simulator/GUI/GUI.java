072a70e5cbb3d2e96a00ba1944339b4ea254da60
Arne Vlietinck <arne.vlietinck@student.kuleuven.be>
2016-10-22 22:08:44 +0200

    design 1.1

--- Workspace_PenO_CW/PenO Drone Simulation/src/simulator/GUI/GUI.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/simulator/GUI/GUI.java
@@ -2,26 +2,67 @@ package simulator.GUI;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
-import javax.swing.JTextField;
-import java.awt.GridLayout;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class GUI extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private JTextField txtSecondTest;
 
 	/**
 	 * Create the panel.
 	 */
 	public GUI() {
-		setLayout(new GridLayout(2, 2, 0, 0));
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		
-		JButton btnTest = new JButton("Test");
-		add(btnTest);
+		// First button
+		JButton camera1 = new JButton("Camera 1");
+		camera1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Code for action
+			}
+		});
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 0;
+		add(camera1, c);
 		
-		txtSecondTest = new JTextField();
-		txtSecondTest.setText("second test");
-		add(txtSecondTest);
-		txtSecondTest.setColumns(10);
+		// Second button
+		JButton camera2 = new JButton("Camera 2");
+		camera2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// CODE
+			}
+		});c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 1;
+		c.gridy = 0;
+		add(camera2, c);
+		
+		// Speed
+		JLabel speed = new JLabel();
+		speed.setText("Speed: ");
+		c.fill = GridBagConstraints.HORIZONTAL;
+	    //c.ipady = 40;      //make this component tall
+	    c.weightx = 0.0;
+	    c.gridx = 0;
+	    c.gridy = 1;
+	    add(speed, c);
+		
+		// Position
+		JLabel position = new JLabel();
+		position.setText("Position: ");
+		c.fill = GridBagConstraints.HORIZONTAL;
+	    c.ipady = 100;      //make this component tall
+	    c.weightx = 0.0;
+	    c.gridx = 1;
+	    c.gridy = 1;
+	    add(position, c);
 
 	}
 
