8197ebc4bbb5b0e59589c341c207929f2c0b7430
Matthias-Vdh <matthias.vdh@hotmail.com>
2016-10-24 14:21:17 +0200

    Hover functie

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/MoveToTarget.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/MoveToTarget.java
@@ -4,15 +4,14 @@ import java.util.ArrayList;
 import exceptions.EmptyPositionListException;
 import implementedClasses.Drone;
 
-
 public class MoveToTarget {
 
-	public MoveToTarget(){
+	public MoveToTarget() {
 		this.imageCalculations = new ImageCalculations();
 		this.physicsCalculations = new PhysicsCalculations();
 	}
-	
-	public void checkcasespixelsfound(Drone drone, ArrayList leftcamera, ArrayList rightcamera){
+
+	public void checkcasespixelsfound(Drone drone, ArrayList leftcamera, ArrayList rightcamera) {
 		if (leftcamera.isEmpty() && rightcamera.isEmpty())
 			noTargetFound();
 		else if (!leftcamera.isEmpty() && rightcamera.isEmpty())
@@ -22,85 +21,111 @@ public class MoveToTarget {
 		else if (!leftcamera.isEmpty() && !rightcamera.isEmpty())
 			targetVisible(drone);
 	}
-	
-	private float slowYaw; //TODO bepalen in verhouding tot max
-	
-	public void noTargetFound(){
+
+	private float slowYaw; // TODO bepalen in verhouding tot max
+
+	public void noTargetFound() {
 		this.setYawRate(this.slowYaw);
 	}
-	
-	public void leftCameraFoundTarget(){
+
+	public void leftCameraFoundTarget() {
 		this.setYawRate(-this.slowYaw);
 	}
-	
-	public void rightCameraFoundTarget(){
+
+	public void rightCameraFoundTarget() {
 		this.setYawRate(this.slowYaw);
 	}
-	
-	public void targetVisible(Drone drone){
+
+	public void targetVisible(Drone drone) {
 		this.setRollRate(0);
-		int[] centerOfGravity = {0,0};
-		try{
-			//TODO nieuwe imagecalc aanmaken -> opnieuw over foto loopen.. beter arraylist meegeven.
-			centerOfGravity = this.getImageCalculations().getCOG(getImageCalculations().getRedPixels(drone.getLeftCamera())); //enkel linkercamera herhaal voor rechter
-		} catch (EmptyPositionListException exception){
+		int[] centerOfGravity = { 0, 0 };
+		try {
+			// TODO nieuwe imagecalc aanmaken -> opnieuw over foto loopen..
+			// beter arraylist meegeven.
+			centerOfGravity = this.getImageCalculations()
+					.getCOG(getImageCalculations().getRedPixels(drone.getLeftCamera())); // enkel linkercamera herhaal voor rechter
+		} catch (EmptyPositionListException exception) {
 			// geen rode pixels
 		}
-		if (this.getPhysicsCalculations().horizontalAngleDeviation(drone, centerOfGravity) >= this.underBoundary ||	
-				this.getPhysicsCalculations().horizontalAngleDeviation(drone, centerOfGravity) <= this.upperBoundary)
+		if (this.getPhysicsCalculations().horizontalAngleDeviation(drone, centerOfGravity) >= this.underBoundary
+				|| this.getPhysicsCalculations().horizontalAngleDeviation(drone, centerOfGravity) <= this.upperBoundary)
 			this.setYawRate(0);
 		else
 			this.setYawRate(drone.getMaxYawRate());
-	}	
-	
-	private final float underBoundary = -10; //TODO bepalen betere waarde
+	}
+
+	private final float underBoundary = -10; // TODO bepalen betere waarde
 	private final float upperBoundary = 10;
-	
-	public void correctRoll(Drone drone){
-		if(drone.getRoll() >= this.upperBoundary || drone.getRoll()<= this.underBoundary) //TODO mss splitsen voor weinig bijsturen en veel bijsturen
+
+	public void correctRoll(Drone drone) {
+		if (drone.getRoll() >= this.upperBoundary || drone.getRoll() <= this.underBoundary) // TODO mss splitsen  voor weinig  bijsturen  en veel / bijsturen
 			this.setRollRate(drone.getMaxRollRate());
 	}
-	
-	public final ImageCalculations getImageCalculations(){
+
+	public void hover(Drone drone) {
+		if (drone.getPitch() >= this.pitchUpper || drone.getPitch() <= this.pitchUnder) {
+			this.setPitchRate(Math.min(drone.getMaxPitchRate(), -drone.getPitch()));
+		}
+
+		else {
+			this.setThrust(Math.min(drone.getMaxThrust(), drone.getGravity() + drone.getDrag()));
+		}
+
+	}
+
+	private final float pitchUnder = -3; // TODO betere waarden
+	private final float pitchUpper = 3;
+
+	public final ImageCalculations getImageCalculations() {
 		return this.imageCalculations;
 	}
+
 	private final ImageCalculations imageCalculations;
-	
-	public final PhysicsCalculations getPhysicsCalculations(){
+
+	public final PhysicsCalculations getPhysicsCalculations() {
 		return this.physicsCalculations;
 	}
+
 	private final PhysicsCalculations physicsCalculations;
-	
-	
-	
-	public void setPitchRate(float value){
+
+	public void setPitchRate(float value) {
 		this.pitchRate = value;
 	}
-	
-	public float getPitchRate(){
+
+	public float getPitchRate() {
 		return this.pitchRate;
 	}
-	
+
 	private float pitchRate;
-	
-	public void setYawRate(float value){
+
+	public void setYawRate(float value) {
 		this.yawRate = value;
 	}
-	
-	public float getYawRate(){
+
+	public float getYawRate() {
 		return this.yawRate;
 	}
-	
+
 	private float yawRate;
-	
-	public void setRollRate(float value){
+
+	public void setRollRate(float value) {
 		this.rollRate = value;
 	}
-	
-	public float getRollRate(){
+
+	public float getRollRate() {
 		return this.rollRate;
 	}
-	
+
 	private float rollRate;
-	
+
+	public void setThrust(float value) {
+		this.thrust = value;
+	}
+
+	public float getThrust() {
+		return this.thrust;
+	}
+
+	private float thrust;
+
 }
