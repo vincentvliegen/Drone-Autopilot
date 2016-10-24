aaa919cfec244bb00e3bde55db3996cbb2611c80
Bram Vandendriessche <bramvandendriessche96@gmail.com>
2016-10-24 16:53:56 +0200

    DroneCamera as subclass of GeneralCamera

--- Workspace_PenO_CW/PenO Drone Simulation/src/simulator/camera/DroneCamera.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/simulator/camera/DroneCamera.java
@@ -2,25 +2,15 @@ package simulator.camera;
 
 import simulator.objects.SimulationDrone;
 
-public class DroneCamera {
+public class DroneCamera extends GeneralCamera{
 	
-	public DroneCamera(SimulationDrone drone){
-		this.drone = drone;
+	public DroneCamera(float eyeX, float eyeY, float eyeZ, float lookAtX, float lookAtY, float lookAtZ, SimulationDrone drone){
+		super(eyeX, eyeY, eyeZ, lookAtX, lookAtY, lookAtZ);
+		this.setDrone(drone);
 	}
 	
-	public float getEyeX() {
-		return eyeX;
-	}
-	public void setEyeX(float eyeX) {
-		this.eyeX = eyeX;
-	}
 
 	private SimulationDrone drone;
-
-	
-	
-	private float eyeX=0, eyeY=0, eyeZ =0;
-	private float lookAtX=0, lookAtY=0, lookAtZ=0;
 	
 	
 	public SimulationDrone getDrone() {
@@ -31,45 +21,6 @@ public class DroneCamera {
 		this.drone = drone;
 	}
 
-	public float getEyeY() {
-		return eyeY;
-	}
-
-	public void setEyeY(float eyeY) {
-		this.eyeY = eyeY;
-	}
-
-	public float getEyeZ() {
-		return eyeZ;
-	}
-
-	public void setEyeZ(float eyeZ) {
-		this.eyeZ = eyeZ;
-	}
-
-	public float getLookAtX() {
-		return lookAtX;
-	}
-
-	public void setLookAtX(float lookAtX) {
-		this.lookAtX = lookAtX;
-	}
-
-	public float getLookAtY() {
-		return lookAtY;
-	}
-
-	public void setLookAtY(float lookAtY) {
-		this.lookAtY = lookAtY;
-	}
-
-	public float getLookAtZ() {
-		return lookAtZ;
-	}
-
-	public void setLookAtZ(float lookAtZ) {
-		this.lookAtZ = lookAtZ;
-	}
 	
 	
 	
