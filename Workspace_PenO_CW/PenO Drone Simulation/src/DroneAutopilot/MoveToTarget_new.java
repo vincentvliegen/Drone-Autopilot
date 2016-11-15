d2b7a1cd9b0c2d93c7512d8583e57c0c7dd6c046
Arne Vlietinck <arne.vlietinck@student.kuleuven.be>
2016-11-15 17:09:19 +0100

    Review Physics
    Wordt vervolgd

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/MoveToTarget_new.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/MoveToTarget_new.java
@@ -1,5 +1,29 @@
 package DroneAutopilot;
 
+import p_en_o_cw_2016.Drone;
+
 public class MoveToTarget_new {
+	
+	//TODO: setbeginMovementTime bij begin van vliegfunctie.
+
+	private final PhysicsCalculations physicsCalculations;
+	private Drone drone;
+
+	public MoveToTarget_new(Drone drone) {
+		this.setDrone(drone);
+		this.physicsCalculations = new PhysicsCalculations(drone);
+	}
+
+	public final PhysicsCalculations getPhysicsCalculations() {
+		return this.physicsCalculations;
+	}
+
+	public void setDrone(Drone drone) {
+		this.drone = drone;
+	}
+
+	public Drone getDrone() {
+		return this.drone;
+	}
 
 }
--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/PhysicsCalculations_new.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/PhysicsCalculations_new.java
@@ -1,5 +1,92 @@
 package DroneAutopilot;
 
+import java.util.ArrayList;
+
+import p_en_o_cw_2016.Drone;
+
 public class PhysicsCalculations_new {
+	
+	private Drone drone;
+	private float speed;
+	private float beginMovementTime;
+	
+	
+	public PhysicsCalculations_new(Drone drone){
+		this.setDrone(drone);
+		setSpeed(0);
+	}
+	
+	public float getDistance(float[] centerOfGravityL, float[]centerOfGravityR){
+		float depth = this.getDepth(centerOfGravityL, centerOfGravityR);
+		float distance =(float) (depth/(Math.cos(Math.toRadians(this.horizontalAngleDeviation(centerOfGravityL, centerOfGravityR)))*Math.cos(Math.toRadians(this.verticalAngleDeviation(centerOfGravityL)))));
+		return distance;
+	}
+	
+	public float getDepth(float[] centerOfGravityL, float[]centerOfGravityR){
+		float depth=0;
+		try{
+		depth = (this.getDrone().getCameraSeparation() * this.getfocalDistance())/(this.getX1(centerOfGravityL) - this.getX2(centerOfGravityR));
+		depth = Math.abs(depth);
+		} catch(IllegalArgumentException e){
+		}
+		return depth;
+	}
+	
+	public float getfocalDistance(){
+		float focal =(float) ((this.getDrone().getLeftCamera().getWidth()/2) / Math.tan(Math.toRadians(this.getDrone().getLeftCamera().getHorizontalAngleOfView()/2)));
+		return focal;
+	}
+	
+	public float getX1(float[] centerofGravityL){
+		float x1 = centerofGravityL[0] - this.getDrone().getLeftCamera().getWidth()/2;
+		return x1;
+	}
+
+	public float getX2(float[] centerofGravityR){
+		float x2 = centerofGravityR[0] - this.getDrone().getRightCamera().getWidth()/2;
+		return x2;
+	}
+	
+	public float horizontalAngleDeviation(float[] centerOfGravityL, float[] centerOfGravityR){
+		float x = (this.getDepth(centerOfGravityL, centerOfGravityR) * this.getX1(centerOfGravityL)) / this.getfocalDistance();
+		float tanAlfa = (x - this.getDrone().getCameraSeparation()/2) / this.getDepth(centerOfGravityL, centerOfGravityR);
+		return (float) Math.toDegrees(Math.atan(tanAlfa));
+	}
+	
+	public float verticalAngleDeviation(float[] centerOfGravity){
+		return (float) Math.toDegrees(Math.atan(this.getY(centerOfGravity) / this.getfocalDistance()));
+	}
+	
+	public float getY(float[] centerofGravity){
+		float y = ((float) centerofGravity[1] - this.getCameraHeight()/2) ;
+		return y;
+	}
+	
+	
+	public int getCameraHeight(){
+		int height =  (int) Math.round(Math.tan(Math.toRadians(this.getDrone().getLeftCamera().getVerticalAngleOfView()/2))*this.getfocalDistance()*2);
+		return height;
+	}
+	
+	public void setbeginMovementTime(float beginMovementTime){
+		this.beginMovementTime = beginMovementTime;
+	}
+	
+	public void setDrone(Drone drone){
+		this.drone = drone;
+	}
+	
+	public Drone getDrone(){
+		return this.drone;
+	}
+	
+	/**
+	 * @param speed the speed to set
+	 */
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+	
+	
 
 }
