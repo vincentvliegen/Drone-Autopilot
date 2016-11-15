3c76e2ee41b35f4aef2e43c4fe4a1b4f6a422eff
Arne Vlietinck <arne.vlietinck@student.kuleuven.be>
2016-11-15 14:08:00 +0100

    Review AutopilotFactory

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/DroneAutopilotFactory_new.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/DroneAutopilotFactory_new.java
@@ -1,5 +1,28 @@
 package DroneAutopilot;
 
-public class DroneAutopilotFactory_new {
+import DroneAutopilot.GUI.GUI;
+import p_en_o_cw_2016.AutopilotFactory;
+import p_en_o_cw_2016.Drone;
+
+public class DroneAutopilotFactory_new implements AutopilotFactory{
+
+	/** Called by the testbed in the AWT/Swing GUI thread to create and start an Autopilot.
+    At this point, the drone exists in the virtual world, with zero pitch and roll. The
+    Autopilot can request initial camera images and set initial values of thrust and
+    rotation rates. It can also set up a Swing GUI. Simulated time is frozen for the
+    duration of this method call; simulated time starts running after this call returns.
+    The Drone and Camera objects are not thread-safe; calls of methods of these
+    objects should occur only in the AWT/Swing GUI thread. */
+	@Override
+	public DroneAutopilot create(Drone drone) {		
+		DroneAutopilot autopilot = new DroneAutopilot(drone);
+		GUI gui = new GUI();
+		autopilot.getMoveToTarget().setGUI(gui);
+		drone.setThrust(Math.abs(drone.getGravity()*drone.getWeight()));
+		drone.setPitchRate(0);
+		drone.setYawRate(0);
+		drone.setRollRate(0);
+		return autopilot;
+	}
 
 }
