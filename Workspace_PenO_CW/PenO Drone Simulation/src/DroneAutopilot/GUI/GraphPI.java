d7fd18f3025fbaf66901969aeddd8e3909cf5eec
Arne Vlietinck <arne.vlietinck@student.kuleuven.be>
2016-11-23 15:26:08 +0100

    Update en Review Graph

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/GUI/GraphPI.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/GUI/GraphPI.java
@@ -13,7 +13,6 @@ import javax.swing.JFrame;
 
 public class GraphPI extends JFrame{
 
-
 	private PICanvas canvas;
 	private Graphics graphics;
 	private List<Integer> x = new ArrayList<>();
@@ -49,7 +48,6 @@ public class GraphPI extends JFrame{
 	private void initialize() {
 		createJFrame();
 		createCanvas();
-
 		pack();
 		setVisible(true);
 	}
@@ -59,7 +57,6 @@ public class GraphPI extends JFrame{
 		setTitle("PID Graph");
 		setAlwaysOnTop(true);
 		setBounds(100, 300, 800, 500);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public void createCanvas(){
@@ -70,57 +67,44 @@ public class GraphPI extends JFrame{
 	}
 
 	public void update(int x, int y){
-		System.out.println("start update");
-		
 		if(this.x.size()==1){
 			oldTimeValue = x;
 			this.x.add(10);
 			this.y.add(300);
 		}
 		else if(this.x.size()>1){
-			int newX = (x-oldTimeValue)/2;
-			this.x.add(this.x.get(this.x.size()-1)+newX);
+			int intervalX = (x-oldTimeValue)*5;
+			this.x.add(this.x.get(this.x.size()-1)+intervalX);
 			this.y.add(this.y.get(0)-y);
-			oldTimeValue = newX;
+			oldTimeValue = x;
 		} 
 
 		graphics = canvas.getGraphics();
-
-		canvas.paint(graphics);
-		repaint();
-		graphics = canvas.getGraphics();
-
 		canvas.paint(graphics);
 		canvas.setVisible(true);
 		repaint();
-	
-	System.out.println("end update");
-}
-	
-
-
-
-class PICanvas extends Canvas{
-	@Override
-	public void paint(Graphics g){
-		if(x.size()==0){
-			//X-as
-			g.drawLine(10, 300, 700, 300);
-			x.add(10);
-			//Y-as
-			g.drawLine(10, 300, 10, 150);
-			y.add(300);
-		}
-
-		g.setColor(Color.RED);
-		for(int i=0; i<x.size(); i++){
-			System.out.println("x: " + x.get(i));
-			System.out.println("y: " + y.get(i));
-			g.fillRect(x.get(i), y.get(i), 2, 2);
-		}
-		System.out.println("paint");
 	}
 
-}
+
+	class PICanvas extends Canvas{
+		@Override
+		public void paint(Graphics g){
+			if(x.size()==0){
+				//X-as
+				g.drawLine(10, 300, 700, 300);
+				x.add(10);
+				//Y-as
+				g.drawLine(10, 300, 10, 150);
+				y.add(300);
+			}
+
+			g.setColor(Color.RED);
+			for(int i=0; i<x.size(); i++){
+				//System.out.println("x: " + x.get(i));
+				//System.out.println("y: " + y.get(i));
+				g.fillRect(x.get(i), y.get(i), 2, 2);
+			}
+		}
+	}
 }
 
