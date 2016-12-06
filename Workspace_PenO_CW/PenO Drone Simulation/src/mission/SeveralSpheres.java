def52345bfbf4140f67fe2e9617174074c13c355
vincentvliegen1 <vincent.vliegen@student.kuleuven.be>
2016-12-06 16:54:30 +0100

    opkuis severalspheres

--- Workspace_PenO_CW/PenO Drone Simulation/src/mission/SeveralSpheres.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/mission/SeveralSpheres.java
@@ -13,7 +13,7 @@ public class SeveralSpheres extends Mission {
 	private boolean firstTime;
 	private boolean firstOrbAcquired;
 	private boolean secondOrbAcquired;
-	private final static float distanceToArrival = 0.8f;// TODO afstellen
+	private final static float distanceToArrival = 0.8f;// TODO afstellen, straal is 0.5
 
 	public SeveralSpheres(MoveToTarget moveToTarget, Drone drone) {
 		super(moveToTarget, drone);
@@ -25,140 +25,39 @@ public class SeveralSpheres extends Mission {
 	@Override
 	public void execute() {
 		//System.out.println("EXSevSph");
-
 		//System.out.println("is scan gedaan? " + this.getMoveToTarget().getWorldScan().getFinished());
-		if (!this.getMoveToTarget().getWorldScan().getFinished()) {// als de
-																	// scanner
-																	// nog geen
-																	// bol heeft
-																	// gevonden,
-																	// blijf
-																	// zoeken
+		if (!this.getMoveToTarget().getWorldScan().getFinished()) {// als de scanner nog geen bol heeft gevonden, blijf zoeken
 			this.getMoveToTarget().getWorldScan().scan(this.getDrone());
 			this.setFirstTime(true);
 			this.setRefreshCounter(0);
 		} else { // als de scanner gedaan is met zoeken:
 
-			// BEREKENING NAAR WAAR TE BEWEGEN
-			if (isFirstTime()) {// eerste keer: bepaal 1ste en tweede bol TODO
-								// refresh
-				this.getClosestOrbs().determineClosestOrbs(); // bepaal alle
-																// bollen in
-																// zicht
-				try {
-					this.getClosestOrbs().calculateFirstOrb(); // bepaal eerste
-																// bol
-					this.setFirstOrbAcquired(true);
-					try {
-						this.getClosestOrbs().calculateSecondOrb(); // bepaal
-																	// tweede
-																	// bol
-						this.setSecondOrbAcquired(true);
-					} catch (NullPointerException e) {// geen tweede bol
-						this.setSecondOrbAcquired(false);
-					} catch (FirstOrbNotVisibleException e) {
-					} // kan niet
-				} catch (NullPointerException e) {// geen eerste bol
-					this.setFirstOrbAcquired(false);
-					this.setSecondOrbAcquired(false);
-				}
-				this.setFirstTime(false);
-			} else if (!this.getMoveToTarget().isScanning() && this.getRefreshCounter() >= getTimeToRefresh()) {
-				this.setRefreshCounter(0);
-				this.getClosestOrbs().determineClosestOrbs();
-				try {
-					int previousFirst = this.getClosestOrbs().getColorFirstOrb();
-					this.getClosestOrbs().calculateFirstOrb();
-					if (this.getClosestOrbs().getColorFirstOrb() == previousFirst) {
-						if (isSecondOrbAcquired()) {
-							int previousSecond = this.getClosestOrbs().getColorSecondOrb();
-							float previousDistance = this.getClosestOrbs().getDistanceSecondOrb();
-							try {
-								this.getClosestOrbs().calculateSecondOrb();
-							} catch (NullPointerException | FirstOrbNotVisibleException e) {
-							}
-							if(this.getClosestOrbs().getColorSecondOrb() != previousSecond) {
-								if (previousDistance <= this.getClosestOrbs().getDistanceSecondOrb()) {
-									this.getClosestOrbs().setColorSecondOrb(previousSecond);
-									this.getClosestOrbs().setDistanceSecondOrb(previousDistance);
-								}
-							}
-						} else {
-							try {
-								this.getClosestOrbs().calculateSecondOrb();
-								this.setSecondOrbAcquired(true);
-							} catch (NullPointerException | FirstOrbNotVisibleException e) {
-							this.setSecondOrbAcquired(false);}
-						}
-					} else {
-						try {
-							this.getClosestOrbs().calculateSecondOrb();
-							this.setSecondOrbAcquired(true);
-						} catch (NullPointerException | FirstOrbNotVisibleException e) {
-						this.setSecondOrbAcquired(false);}
-					}
-
-				} catch (NullPointerException e) {// geen eerste bol
-					
-				}
-			} else {// elke keer behalve de eerste
-				float[] cogL = this.getMoveToTarget().getCogL(); // van vorige
-																	// cyclus
-				float[] cogR = this.getMoveToTarget().getCogR(); // van vorige
-																	// cyclus
+			// BEREKENING BOLLEN
+			if (isFirstTime()) {// eerste keer nadat de scan gedaan is
+				firstTimeSinceScan();
+			} else if (!this.getMoveToTarget().isScanning() && this.getRefreshCounter() >= getTimeToRefresh()) {// wanneer de drone de 1ste bol in zicht heeft en er naar toe vliegt, herbereken de eerste en 2de bol op geregelde tijdstippen
+				refreshWhenFlying();
+			} 
+			if(!isFirstTime()) {// kijk of we aangekomen zijn in de 1ste bol en hernieuw target (elke keer behalve de eerste keer na scannen)
+				float[] cogL = this.getMoveToTarget().getCogL();
+				float[] cogR = this.getMoveToTarget().getCogR();
 				float distance = this.getClosestOrbs().getPhysicsCalculations().getDistance(cogL, cogR);
 				//System.out.println("distance = " + distance);
-				// wanneer de eerste bol gepasseerd is, ga verder naar de tweede
-				if (distance <= getDistancetoarrival()
+				
+				if (distance <= getDistancetoarrival()// wanneer de 1ste bol bereikt wordt (binnen afstand of (bijna) alles gekleurd)
 						|| this.getClosestOrbs().getImageCalculations().checkImageFilled(
 								this.getDrone().getLeftCamera(), this.getClosestOrbs().getColorFirstOrb())
 						|| this.getClosestOrbs().getImageCalculations().checkImageFilled(
-								this.getDrone().getRightCamera(), this.getClosestOrbs().getColorFirstOrb())) {// wanneer
-																												// de
-																												// bol
-																												// bereikt
-																												// wordt
-					if (isSecondOrbAcquired()) {
-						// verander 2de in eerste orb
-						int previousFirstOrb = this.getClosestOrbs().getColorFirstOrb();
-						this.getClosestOrbs().setColorFirstOrb(this.getClosestOrbs().getColorSecondOrb());
-						setFirstOrbAcquired(true);
-
-						// bereken 2de orb
-						this.getClosestOrbs().determineClosestOrbs();
-						this.getClosestOrbs().getClosestOrbs().remove(previousFirstOrb);// verwijdert
-																						// de
-																						// oorspronnkelijke
-																						// eerste
-																						// als
-																						// deze
-																						// nog
-																						// zichtbaar
-																						// is
-						if (this.getClosestOrbs().getClosestOrbs().size() > 1) {
-							try {
-								this.getClosestOrbs().calculateSecondOrb();// bepaal
-																			// nieuwe
-																			// tweede
-								this.setSecondOrbAcquired(true);
-							} catch (NullPointerException | FirstOrbNotVisibleException e) {
-								this.setSecondOrbAcquired(false);
-							}
-						} else {
-							this.setSecondOrbAcquired(false);
-						}
-					} else {
-						this.setFirstOrbAcquired(false);
-					}
+								this.getDrone().getRightCamera(), this.getClosestOrbs().getColorFirstOrb())) {
+					ArrivedAtTarget();
 				}
 			}
-			//System.out.println("firstOrb " + this.getClosestOrbs().getColorFirstOrb());
 
 			// BEWEGING
-			if (isFirstOrbAcquired()) {
+			if (isFirstOrbAcquired()) {//vlieg naar de eerste bol als we deze weten
 				this.getMoveToTarget().execute(this.getClosestOrbs().getColorFirstOrb()); 
 				this.setRefreshCounter(this.getRefreshCounter() + 1);
-			} else {// begin opnieuw te zoeken
+			} else {// begin opnieuw te zoeken naar eerste bol
 				//System.out.println("herstart scan");
 				this.getMoveToTarget().getWorldScan().scan(this.getDrone());
 				setFirstTime(true);
@@ -166,6 +65,90 @@ public class SeveralSpheres extends Mission {
 		}
 	}
 
+	public void firstTimeSinceScan(){
+		this.getClosestOrbs().determineClosestOrbs(); // bepaal alle bollen in zicht
+		try {
+			this.getClosestOrbs().calculateFirstOrb(); // bepaal eerste bol
+			this.setFirstOrbAcquired(true);
+			try {
+				this.getClosestOrbs().calculateSecondOrb(); // bepaal tweede bol
+				this.setSecondOrbAcquired(true);
+			} catch (NullPointerException e) {// geen tweede bol
+				this.setSecondOrbAcquired(false);
+			} catch (FirstOrbNotVisibleException e) {
+			} // kan niet
+		} catch (NullPointerException e) {// geen eerste bol
+			this.setFirstOrbAcquired(false);
+			this.setSecondOrbAcquired(false);
+		}
+		this.setFirstTime(false);// hierna is het niet meer de eerste keer
+	}
+	
+	public void refreshWhenFlying(){
+		this.setRefreshCounter(0);
+		this.getClosestOrbs().determineClosestOrbs();//bereken dichtstbijzijnde bollen
+		try {//bereken eerste bol
+			int previousFirst = this.getClosestOrbs().getColorFirstOrb();
+			this.getClosestOrbs().calculateFirstOrb();//bereken eerste bol
+			if (this.getClosestOrbs().getColorFirstOrb() == previousFirst) {//eerste bol blijft eerste bol 
+				if (isSecondOrbAcquired()) {//is er een tweede bol?
+					int previousSecond = this.getClosestOrbs().getColorSecondOrb();
+					float previousDistance = this.getClosestOrbs().getDistanceSecondOrb();
+					try {
+						this.getClosestOrbs().calculateSecondOrb();
+					} catch (NullPointerException | FirstOrbNotVisibleException e) {//tweede bol is reeds berekend, dus geen bol in zicht doet niets | eerste bol is in zicht
+					}
+					if(this.getClosestOrbs().getColorSecondOrb() != previousSecond) {//is tweede bol anders?
+						if (previousDistance <= this.getClosestOrbs().getDistanceSecondOrb()) {//kijk of de nieuwe tweede bol dichterbij ligt dan de vorige
+							this.getClosestOrbs().setColorSecondOrb(previousSecond);
+							this.getClosestOrbs().setDistanceSecondOrb(previousDistance);
+						}//geen else, tweede bol blijft
+					}
+				} else {//geen tweede bol, kijk of er toch een is
+					try {
+						this.getClosestOrbs().calculateSecondOrb();
+						this.setSecondOrbAcquired(true);
+					} catch (NullPointerException | FirstOrbNotVisibleException e) {
+					this.setSecondOrbAcquired(false);}
+				}
+			} else {//eerste bol verandert
+				try {
+					this.getClosestOrbs().calculateSecondOrb();//nieuwe tweede bol bij de nieuwe eerste bol
+					this.setSecondOrbAcquired(true);
+				} catch (NullPointerException | FirstOrbNotVisibleException e) {
+				this.setSecondOrbAcquired(false);}
+			}
+
+		} catch (NullPointerException e) {// geen eerste bol (kan niet wanneer we niet aan het zoeken zijn)
+			
+		}
+	}
+	
+	public void ArrivedAtTarget(){
+		if (isSecondOrbAcquired()) {//is er een tweede bol berekend?
+			// verander 2de in eerste orb
+			int previousFirstOrb = this.getClosestOrbs().getColorFirstOrb();
+			this.getClosestOrbs().setColorFirstOrb(this.getClosestOrbs().getColorSecondOrb());
+			setFirstOrbAcquired(true);
+
+			// bereken 2de orb
+			this.getClosestOrbs().determineClosestOrbs();
+			this.getClosestOrbs().getClosestOrbs().remove(previousFirstOrb);// verwijdert de eerste als deze nog zichtbaar is
+			if (this.getClosestOrbs().getClosestOrbs().size() > 1) {
+				try {
+					this.getClosestOrbs().calculateSecondOrb();// bepaal nieuwe tweede
+					this.setSecondOrbAcquired(true);
+				} catch (NullPointerException | FirstOrbNotVisibleException e) {
+					this.setSecondOrbAcquired(false);
+				}
+			} else {
+				this.setSecondOrbAcquired(false);
+			}
+		} else {//geen eerste bol
+			this.setFirstOrbAcquired(false);
+		}
+	}
+	
 	/**
 	 * @return the refreshCounter
 	 */
