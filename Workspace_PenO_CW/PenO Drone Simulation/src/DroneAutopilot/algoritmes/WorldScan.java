78259fb93512ab09eaf1cfce36eaac537d781dce
Laura Vranken <lauravranken96@gmail.com>
2016-12-15 15:08:43 +0100

    gui kommagetallen, nan bij distance, scan verbetert met gravity

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/algoritmes/WorldScan.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/algoritmes/WorldScan.java
@@ -106,17 +106,21 @@ public class WorldScan {
 	}
 
 	public boolean scan(ArrayList<int[]> leftCameraList, ArrayList<int[]> rightCameraList, int color){
+		//System.out.println("scan move to");
 		boolean canStartFly = false;
 		ArrayList<Boolean> targetFound = this.checkCasesPixelsFound(leftCameraList, rightCameraList);
 
 		if(targetFound.get(0)==false && targetFound.get(1)==true){
 			canStartFly = false;
+			this.rightCameraFoundTarget();
 		}else if(targetFound.get(0)==true && targetFound.get(1)==false){
 			canStartFly = false;
+			this.leftCameraFoundTarget();
 		}else if(targetFound.get(0)==true && targetFound.get(1)==true){
 			canStartFly = true;
 		}else if(targetFound.get(0)==false && targetFound.get(1)==false){
 			canStartFly = false;
+			this.noTargetFound();
 		}
 		return canStartFly;
 	}
@@ -147,14 +151,20 @@ public class WorldScan {
 
 	public void noTargetFound() {
 		this.getDrone().setYawRate(this.getYawrate());
+		float gravity = Math.abs(this.getDrone().getGravity())*this.getDrone().getWeight();
+		this.getDrone().setThrust(gravity/ (float) Math.cos(Math.toRadians(this.getDrone().getPitch())));
 	}
 
 	public void leftCameraFoundTarget() {
 		this.getDrone().setYawRate(-this.getYawrate());
+		float gravity = Math.abs(this.getDrone().getGravity())*this.getDrone().getWeight();
+		this.getDrone().setThrust(gravity/ (float) Math.cos(Math.toRadians(this.getDrone().getPitch())));
 	}
 
 	public void rightCameraFoundTarget() {
 		this.getDrone().setYawRate(this.getYawrate());
+		float gravity = Math.abs(this.getDrone().getGravity())*this.getDrone().getWeight();
+		this.getDrone().setThrust(gravity/ (float) Math.cos(Math.toRadians(this.getDrone().getPitch())));
 	}
 
 
