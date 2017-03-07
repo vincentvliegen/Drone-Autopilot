5b0ca475a9b2f778a7626ab572fc14b5531c7716
Jef Versyck <r0578402@heist.cs.kotnet.kuleuven.be.>
2017-03-07 18:26:24 +0100

    GeneratorV2 updated to have random nr of SomeFigures
    will change this to random PredefinedPolyhedron later

--- Workspace_PenO_CW/PenO Drone Simulation/src/generator/Generator_V2.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/generator/Generator_V2.java
@@ -9,6 +9,8 @@ import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.util.*;
 
+import simulator.physics.MathCalculations;
+
 public class Generator_V2 {
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -63,32 +65,54 @@ public class Generator_V2 {
 			}
 
 			// Objects
-			stream.writeShort(2);
+			int numberOfObjects = r.nextInt(7) + 5;
+			int numberOfObstacles = numberOfObjects / 2;
+			int numberOfTargets = numberOfObjects - numberOfObstacles;
+			stream.writeShort(numberOfObjects);
 
-			// Object 1 == target
-			stream.writeShort(4);
-			// Locations of points
-			stream.writeFloat(4.8f);
-			stream.writeFloat(0);
-			stream.writeFloat(-0.2f);
+			// Calculate all positions
+			List<double[]> positionList = new ArrayList<>();
+			positionList.add(new double[] { 0, 0, 0 });
 
-			stream.writeFloat(4.8f);
-			stream.writeFloat(0);
-			stream.writeFloat(0.2f);
+			for (int i = 0; i < numberOfObjects; i++) {
+				boolean noValidNewPosition = true;
+				while (noValidNewPosition) {
+					double x = (r.nextDouble() - .5) * 30;
+					double y = (r.nextDouble() - .5) * 30;
+					double z = (r.nextDouble() - .5) * 30;
+					double[] newPos = { x, y, z };
+					noValidNewPosition = false;
+					for (double[] otherPosition : positionList) {
+						// Distance > 3*drone diameter (=1.5)
+						if (MathCalculations.getDistanceBetweenPoints(
+								otherPosition, newPos) <= 1.5) {
+							noValidNewPosition = true;
+							break;
+						}
+						// Check if the vectors are linearly dependent (not
+						// needed for {0,0,0})
+						if ((otherPosition[0] != 0) || (otherPosition[1] != 0)
+								|| (otherPosition[2] != 0)) {
+							if ((x * otherPosition[1] - y * otherPosition[0]) == 0
+									&& (x * otherPosition[2] - z
+											* otherPosition[0]) == 0
+									&& (y * otherPosition[2] - z
+											* otherPosition[1]) == 0) {
+								noValidNewPosition = true;
+								break;
+							}
 
-			stream.writeFloat(5.2f);
-			stream.writeFloat(0);
-			stream.writeFloat(0);
+						}
+					}
+					if (!noValidNewPosition) {
+						positionList.add(newPos);
+					}
+				}
+			}
 
-			stream.writeFloat(5);
-			stream.writeFloat(0.2f);
-			stream.writeFloat(0);
-
-			stream.writeShort(4);
-			// Connect points + colors
-
+			// create target colors
 			List<int[]> colorListRGB = new ArrayList<>();
-			for (int i = 0; i < 4; i++) {
+			for (int i = 0; i < numberOfTargets * 4; i++) {
 				int hue = r.nextInt(361);
 				float saturation = r.nextFloat() * 0.45f + 0.55f;
 				float brightness = r.nextFloat() * 0.45f + 0.55f;
@@ -100,67 +124,75 @@ public class Generator_V2 {
 				colorListRGB.add(color);
 			}
 
-			for (int i = 0; i < 4; i++) {
-				int[] currentcolor = colorListRGB.get(i);
-				switch (i) {
-				case 0:
-					stream.writeShort(0);
-					stream.writeShort(1);
-					stream.writeShort(2);
-					stream.writeByte(currentcolor[0]);
-					stream.writeByte(currentcolor[1]);
-					stream.writeByte(currentcolor[2]);
-					break;
-				case 1:
-					stream.writeShort(0);
-					stream.writeShort(1);
-					stream.writeShort(3);
-					stream.writeByte(currentcolor[0]);
-					stream.writeByte(currentcolor[1]);
-					stream.writeByte(currentcolor[2]);
-					break;
-				case 2:
-					stream.writeShort(0);
-					stream.writeShort(3);
-					stream.writeShort(2);
-					stream.writeByte(currentcolor[0]);
-					stream.writeByte(currentcolor[1]);
-					stream.writeByte(currentcolor[2]);
-					break;
-				case 3:
-					stream.writeShort(1);
-					stream.writeShort(3);
-					stream.writeShort(2);
-					stream.writeByte(currentcolor[0]);
-					stream.writeByte(currentcolor[1]);
-					stream.writeByte(currentcolor[2]);
-					break;
+			// Target objects
+			int currentPosIndex = 0;
+			for (int i = 0; i < numberOfTargets; i++) {
+				stream.writeShort(4);
+				float x = (float) positionList.get(currentPosIndex)[0];
+				float y = (float) positionList.get(currentPosIndex)[1];
+				float z = (float) positionList.get(currentPosIndex)[2];
+
+				stream.writeFloat(-0.2f + x);
+				stream.writeFloat(y);
+				stream.writeFloat(-0.2f + z);
+
+				stream.writeFloat(-0.2f + x);
+				stream.writeFloat(y);
+				stream.writeFloat(0.2f + z);
+
+				stream.writeFloat(0.2f + x);
+				stream.writeFloat(y);
+				stream.writeFloat(z);
+
+				stream.writeFloat(x);
+				stream.writeFloat(y + 0.2f);
+				stream.writeFloat(z);
+
+				stream.writeShort(4);
+
+				for (int j = 0; j < 4 * numberOfTargets; j++) {
+					int[] currentcolor = colorListRGB.get(j);
+					switch (j) {
+					case 0:
+						stream.writeShort(0);
+						stream.writeShort(1);
+						stream.writeShort(2);
+						stream.writeByte(currentcolor[0]);
+						stream.writeByte(currentcolor[1]);
+						stream.writeByte(currentcolor[2]);
+						break;
+					case 1:
+						stream.writeShort(0);
+						stream.writeShort(1);
+						stream.writeShort(3);
+						stream.writeByte(currentcolor[0]);
+						stream.writeByte(currentcolor[1]);
+						stream.writeByte(currentcolor[2]);
+						break;
+					case 2:
+						stream.writeShort(0);
+						stream.writeShort(3);
+						stream.writeShort(2);
+						stream.writeByte(currentcolor[0]);
+						stream.writeByte(currentcolor[1]);
+						stream.writeByte(currentcolor[2]);
+						break;
+					case 3:
+						stream.writeShort(1);
+						stream.writeShort(3);
+						stream.writeShort(2);
+						stream.writeByte(currentcolor[0]);
+						stream.writeByte(currentcolor[1]);
+						stream.writeByte(currentcolor[2]);
+						break;
+					}
 				}
+
 			}
 
 			colorListRGB.clear();
-			// Object 2 == obstacle
-			stream.writeShort(4);
-
-			stream.writeFloat(-0.2f);
-			stream.writeFloat(5);
-			stream.writeFloat(-0.2f);
-
-			stream.writeFloat(-0.2f);
-			stream.writeFloat(5);
-			stream.writeFloat(0.2f);
-
-			stream.writeFloat(0.2f);
-			stream.writeFloat(5);
-			stream.writeFloat(0);
-
-			stream.writeFloat(0);
-			stream.writeFloat(5.2f);
-			stream.writeFloat(0);
-
-			stream.writeShort(4);
-
-			for (int i = 0; i < 4; i++) {
+			// Obstacle
+			for (int i = 0; i < 4 * numberOfObstacles; i++) {
 				int hue = r.nextInt(361);
 				float saturation = r.nextFloat() * 0.45f + 0.55f;
 				float brightness = r.nextFloat() * 0.45f;
@@ -172,43 +204,70 @@ public class Generator_V2 {
 				colorListRGB.add(color);
 			}
 
-			for (int i = 0; i < 4; i++) {
-				int[] currentcolor = colorListRGB.get(i);
-				switch (i) {
-				case 0:
-					stream.writeShort(0);
-					stream.writeShort(1);
-					stream.writeShort(2);
-					stream.writeByte(currentcolor[0]);
-					stream.writeByte(currentcolor[1]);
-					stream.writeByte(currentcolor[2]);
-					break;
-				case 1:
-					stream.writeShort(0);
-					stream.writeShort(1);
-					stream.writeShort(3);
-					stream.writeByte(currentcolor[0]);
-					stream.writeByte(currentcolor[1]);
-					stream.writeByte(currentcolor[2]);
-					break;
-				case 2:
-					stream.writeShort(0);
-					stream.writeShort(3);
-					stream.writeShort(2);
-					stream.writeByte(currentcolor[0]);
-					stream.writeByte(currentcolor[1]);
-					stream.writeByte(currentcolor[2]);
-					break;
-				case 3:
-					stream.writeShort(1);
-					stream.writeShort(3);
-					stream.writeShort(2);
-					stream.writeByte(currentcolor[0]);
-					stream.writeByte(currentcolor[1]);
-					stream.writeByte(currentcolor[2]);
-					break;
+			for (int i = 0; i < numberOfObstacles; i++) {
+				stream.writeShort(4);
+				float x = (float) positionList.get(currentPosIndex)[0];
+				float y = (float) positionList.get(currentPosIndex)[1];
+				float z = (float) positionList.get(currentPosIndex)[2];
+
+				stream.writeFloat(-0.2f + x);
+				stream.writeFloat(y);
+				stream.writeFloat(-0.2f + z);
+
+				stream.writeFloat(-0.2f + x);
+				stream.writeFloat(y);
+				stream.writeFloat(0.2f + z);
+
+				stream.writeFloat(0.2f + x);
+				stream.writeFloat(y);
+				stream.writeFloat(z);
+
+				stream.writeFloat(x);
+				stream.writeFloat(y + 0.2f);
+				stream.writeFloat(z);
+
+				stream.writeShort(4);
+
+				for (int j = 0; j < 4 * numberOfObstacles; j++) {
+					int[] currentcolor = colorListRGB.get(j);
+					switch (j) {
+					case 0:
+						stream.writeShort(0);
+						stream.writeShort(1);
+						stream.writeShort(2);
+						stream.writeByte(currentcolor[0]);
+						stream.writeByte(currentcolor[1]);
+						stream.writeByte(currentcolor[2]);
+						break;
+					case 1:
+						stream.writeShort(0);
+						stream.writeShort(1);
+						stream.writeShort(3);
+						stream.writeByte(currentcolor[0]);
+						stream.writeByte(currentcolor[1]);
+						stream.writeByte(currentcolor[2]);
+						break;
+					case 2:
+						stream.writeShort(0);
+						stream.writeShort(3);
+						stream.writeShort(2);
+						stream.writeByte(currentcolor[0]);
+						stream.writeByte(currentcolor[1]);
+						stream.writeByte(currentcolor[2]);
+						break;
+					case 3:
+						stream.writeShort(1);
+						stream.writeShort(3);
+						stream.writeShort(2);
+						stream.writeByte(currentcolor[0]);
+						stream.writeByte(currentcolor[1]);
+						stream.writeByte(currentcolor[2]);
+						break;
+					}
 				}
+
 			}
+
 			stream.close();
 		} catch (Exception e) {
 			e.printStackTrace();
