d4e474cb752ad1752e2d4a48dc78bb546911cfe7
Arne Vlietinck <arne.vlietinck@student.kuleuven.be>
2017-03-16 22:01:37 +0100

    Hover
    Implementatie om ter plaatse te blijven.

--- Workspace_PenO_CW/PenO Drone Simulation/src/mission/Hover.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/mission/Hover.java
@@ -4,13 +4,18 @@ import DroneAutopilot.DroneAutopilot;
 
 public class Hover extends Mission{
 	
+	public float[] previousPosition;
+	
 	public Hover(DroneAutopilot droneAutopilot){
 		super(droneAutopilot);
+		this.setPreviousPosition(new float[] {this.getDrone().getX(), this.getDrone().getY(), this.getDrone().getZ()}); 
 	}
 
 	@Override
 	public void execute() {
-		this.getPhysicsCalculations().updateOrientation(this.getPhysicsCalculations().getDirectionOfView());//blijf dezelfde richting kijken
+		this.getPhysicsCalculations().updatePosition(this.getPreviousPosition()); //blijf opdezelfde plaats
+		this.setPreviousPosition(new float[] {this.getDrone().getX(), this.getDrone().getY(), this.getDrone().getZ()});
+		//this.getPhysicsCalculations().updateOrientation(this.getPhysicsCalculations().getDirectionOfView());//blijf dezelfde richting kijken
 	}
 
 	@Override
@@ -18,5 +23,17 @@ public class Hover extends Mission{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	//////////GETTERS & SETTERS//////////
+
+	public float[] getPreviousPosition() {
+		return previousPosition;
+	}
+
+	public void setPreviousPosition(float[] previousPosition) {
+		this.previousPosition = previousPosition;
+	}
+	
+	
 
 }
