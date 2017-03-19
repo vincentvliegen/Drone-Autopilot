66d01ff0880025b21f5e62e5dae82455d04a9b9d
Bram Vandendriessche <bramvandendriessche96@gmail.com>
2017-03-19 12:36:36 +0100

    Scanning: graphical representation

---
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/graphicalrepresentation/TriangleAPData.java
@@ -0,0 +1,85 @@
+package DroneAutopilot.graphicalrepresentation;
+
+/**
+ * Data-aspect of the Triangles, containing the coordinates and the colors of the Triangle
+ *
+ */
+public class TriangleAPData {
+	
+	private double[] p1;
+	private double[] p2;
+	private double[] p3;
+	private int[] rgbInner;
+	private int[] rgbOuter;
+	private double[] innerPoint1;
+	private double[] innerPoint2;
+	private double[] innerPoint3;
+
+	public TriangleAPData(double[] p1, double[] p2, double[] p3, int[] rgbInner, int[] rgbOuter) {
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+		innerPoint1 = getInnerPoint(getPoint1());
+		innerPoint2 = getInnerPoint(getPoint2());
+		innerPoint3 = getInnerPoint(getPoint3());
+		//TODO colors in which format, might be needed to adjust this
+		this.rgbInner = rgbInner;
+		this.rgbOuter = rgbOuter;
+	}
+
+	public double[] getPoint1() {
+		return p1;
+	}
+	
+	public double[] getPoint2() {
+		return p2;
+	}
+	
+	public double[] getPoint3() {
+		return p3;
+	}
+	
+	public int[] getRgbInner() {
+		return rgbInner;
+	}
+	
+	public int[] getRgbOuter() {
+		return rgbOuter;
+	}
+	
+	public double[] getInnerPoint1() {
+		return innerPoint1;
+	}
+
+	public double[] getInnerPoint2() {
+		return innerPoint2;
+	}
+
+	public double[] getInnerPoint3() {
+		return innerPoint3;
+	}
+	
+	private double getGravityX() {
+		return (getPoint1()[0] + getPoint2()[0] + getPoint3()[0]) / 3;
+
+	}
+
+	private double getGravityY() {
+		return (getPoint1()[1] + getPoint2()[1] + getPoint3()[1]) / 3;
+
+	}
+
+	private double getGravityZ() {
+		return (getPoint1()[2] + getPoint2()[2] + getPoint3()[2]) / 3;
+
+	}
+
+
+	private double[] getInnerPoint(double[] outerPoint) {
+		return new double[] { (outerPoint[0] - getGravityX()) / Math.sqrt(2) + getGravityX(),
+				(outerPoint[1] - getGravityY()) / Math.sqrt(2) + getGravityY(),
+				(outerPoint[2] - getGravityZ()) / Math.sqrt(2) + getGravityZ() };
+	}
+	
+	
+}
---
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/graphicalrepresentation/WorldAP.java
@@ -0,0 +1,19 @@
+package DroneAutopilot.graphicalrepresentation;
+
+public class WorldAP {
+
+	/*
+	 * Idee: dit als klasse voor openGL (opsplitsing tussen GL-gedeelte en
+	 * data-gedeelte bevat gescande objecten (en evt. een kleine sphere als
+	 * drone-object om aan te geven waar de drone zich bevindt kunnen dan ahv
+	 * missie een camera selecteren (het te scannen object weergeven, evt. al
+	 * roterend, of algemeen 2D/3D beeld van wat reeds gescand is)
+	 * 
+	 */
+
+	/*
+	 * De wereld bevat: lijst met polyhedrons evt een drone functies voor het
+	 * setten van camera een GL-wereld met init etc. ......
+	 */
+
+}
