b26c211a0106a75f52318b3bad829d4fc323c0fb
Vincent Vliegen <vincent>
2017-03-26 21:47:50 +0200

    fix bgcos( 1< ) in yawrate

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/calculations/PhysicsCalculations.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/calculations/PhysicsCalculations.java
@@ -48,7 +48,7 @@ public class PhysicsCalculations{
 	private static final float[] maxWindRotationRate = new float[]{(float) 0.5,(float) 0.5,(float) 0.5};
 	private boolean firstMovement;
 	private final static float distanceSpeedFactor = 0.5f;
-	private final static float dropdownDistance = 2f;
+	private final static float dropdownDistance = 0.5f;
 	
 	//Vector Rotations
 	private float[][] rotationMatrix;
@@ -565,29 +565,26 @@ public class PhysicsCalculations{
 		//Geeft de nog te overbruggen hoeken richting het object weer. Yaw & Pitch & Roll
 		private void calculateRemainingAnglesToObject() throws ArithmeticException{
 			float[] thrustWanted = VectorCalculations.normalise(this.getWantedOrientation()[0]);
-			float[] thrustWantedDrone = this.vectorWorldToDrone(thrustWanted);
 			float[] viewWanted = VectorCalculations.normalise(this.getWantedOrientation()[1]);
-			float[] viewWantedDrone = this.vectorWorldToDrone(viewWanted);
 			//DroneAngles gaan niet meer nodig zijn, aangezien we gwn de nodige hoek berekenen. //TODO
 			float[] inverseDroneAngles = new float[] {-this.getDrone().getHeading(), -this.getDrone().getPitch(), -this.getDrone().getRoll()};
 			
-			// TODO: Hoek tussen de projectie van de viewvector op het YZ vlak en negatieve z as. 
 			float pitchWanted = (float) Math.toDegrees(Math.asin(thrustWanted[2]));
 			if (pitchWanted == Math.PI){
 				throw new ArithmeticException("pitchWanted is PI, division by zero");
 			}
-			//TODO: Roll: Hoek tussen de projectie van de thrustvector op het XY vlak vd drone en de y-as.
 			//float rollWanted = (float) Math.toDegrees(-Math.acos(thrustWanted[1]/Math.cos(Math.toRadians(pitchWanted))));  Alles in stukken gehakt, waardoor geen NaN meer.
-				float value1 = (float) Math.cos(Math.toRadians(pitchWanted));
-				float value2 = (float) (thrustWanted[1]/value1);
-				float value3;
+			float cosPitch = (float) Math.cos(Math.toRadians(pitchWanted));
+				float value1 = (float) (thrustWanted[1]/cosPitch);
+				if (Math.abs(value1) > 1){
+					value1 = 1*Math.signum(value1);
+				}
+			float rollWanted = (float) Math.toDegrees(-Math.acos(value1));
+				float value2 = -viewWanted[2]/cosPitch;
 				if (Math.abs(value2) > 1){
-					value3 = 1*Math.signum(value2);
-				} else {
-					value3 = (float) -Math.acos(value2);}
-				float rollWanted = (float) Math.toDegrees(value3);
-			//TODO: Yaw: Hoek tussen de projectie van de viewvector op XZ vlak vd drone en de negatieve z-as (kijkrichting vd drone).
-			float yawWanted = (float) Math.toDegrees(Math.acos(-viewWanted[2]/Math.cos(Math.toRadians(pitchWanted))));
+					value2 = 1*Math.signum(value2);
+				}
+			float yawWanted = (float) Math.toDegrees(Math.acos(value2));
 			float[] wantedDroneAngles = new float[] {yawWanted, pitchWanted, rollWanted};
 			float[] remainingAngles = VectorCalculations.sum(inverseDroneAngles, wantedDroneAngles);
 			this.setRemainingAngles(remainingAngles);
