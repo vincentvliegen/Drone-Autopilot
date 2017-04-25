aa344315e3206d3fc0c5f58e62c3339ae43c7715
Bram Vandendriessche <bramvandendriessche96@gmail.com>
2017-04-25 18:22:22 +0200

    WindowForEdgeTesting

---
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/graphicalrepresentation/WindowForEdgeMatching.java
@@ -0,0 +1,38 @@
+package DroneAutopilot.graphicalrepresentation;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.ArrayList;
+
+class WindowForEdgeMatching extends JFrame{
+
+    public WindowForEdgeMatching() {
+        JPanel panel=new JPanel();
+        getContentPane().add(panel);
+        setSize(450,450);
+
+        JButton button =new JButton("press");
+        panel.add(button);
+    }
+    
+    public ArrayList<ArrayList<double[]>> myList;
+    
+    public void setMyList(ArrayList<ArrayList<double[]>> myList) {
+		this.myList = myList;
+	}
+
+    public void paint(Graphics g) {
+        super.paint(g);  // fixes the immediate problem.
+        Graphics2D g2 = (Graphics2D) g;
+        for(ArrayList<double[]> arrlist : myList) {
+    		Line2D lin = new Line2D.Double(arrlist.get(0)[2], arrlist.get(0)[1], arrlist.get(1)[2], arrlist.get(1)[1]);
+    		g2.draw(lin);
+        }
+        
+        Line2D lin = new Line2D.Float(100, 100, 250, 260);
+        g2.draw(lin);
+    }
+
+
+}
