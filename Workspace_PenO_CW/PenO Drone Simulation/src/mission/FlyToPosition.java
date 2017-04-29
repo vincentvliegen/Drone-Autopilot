c1997d8acc6d1532ad968edfc66fec0811df7be2
Vincent Vliegen <vincent>
2017-04-29 13:23:35 +0200

    prints/locatie breakpoint voor Jef

--- Workspace_PenO_CW/PenO Drone Simulation/src/mission/FlyToPosition.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/mission/FlyToPosition.java
@@ -10,12 +10,20 @@ public class FlyToPosition extends Mission{
 	
 	public FlyToPosition(DroneAutopilot droneAutopilot) {
 		super(droneAutopilot);
-		this.setTarget(new double[] {0,-1,0});
+		this.setTarget(new double[] {25,0,0});
 	}
 	
 	@Override
 	public void execute() {
 		this.getPhysicsCalculations().updatePosition(this.getTarget());
+		if(getPhysicsCalculations().getRollRate() != 0){
+			System.out.println("currentYaw: " + this.getPhysicsCalculations().getDrone().getHeading());
+			System.out.println("currentPitch: " + this.getPhysicsCalculations().getDrone().getPitch());
+			System.out.println("currentRoll: " + this.getPhysicsCalculations().getDrone().getRoll());	
+			System.out.println("yawrate: " + this.getPhysicsCalculations().getYawRate());
+			System.out.println("pitchrate: " + this.getPhysicsCalculations().getPitchRate());
+			System.out.println("rollrate: " + this.getPhysicsCalculations().getRollRate());
+		}
 	}
 
 	@Override
