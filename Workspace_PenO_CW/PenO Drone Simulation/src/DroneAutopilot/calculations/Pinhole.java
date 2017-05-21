6167b16e5436f092cdbda80867e23435b80b2618
Bram Vandendriessche <bramvandendriessche96@gmail.com>
2017-05-21 14:29:43 +0200

    Pinhole update - (not working)

--- Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/calculations/Pinhole.java
+++ Workspace_PenO_CW/PenO Drone Simulation/src/DroneAutopilot/calculations/Pinhole.java
@@ -7,118 +7,129 @@ import DroneAutopilot.DroneAutopilot;
 import Jama.Matrix;
 
 public class Pinhole {
-	
-	
+
 	private DroneAutopilot ap;
-	private Matrix leftC;
-	private Matrix rightC;
 	private Matrix leftR1;
 	private Matrix leftR2;
 	private Matrix leftR3;
 	private Matrix rightR1;
 	private Matrix rightR2;
 	private Matrix rightR3;
-
+	private double[][] globalpoint;
 
 	public Pinhole(DroneAutopilot ap) {
 		this.ap = ap;
 		setup();
 	}
-	
+
 	private void setup() {
-		double horizfov = ap.getDrone().getLeftCamera().getHorizontalAngleOfView();
-		double vertfov = ap.getDrone().getLeftCamera().getVerticalAngleOfView();
 		int cameraWidth = ap.getDrone().getLeftCamera().getWidth();
-		int cameraHeight = (int) (ap.getDrone().getLeftCamera().takeImage().length/cameraWidth);
+		int cameraHeight = (int) (ap.getDrone().getLeftCamera().takeImage().length / cameraWidth);
 		double f = ap.getPhysicsCalculations().getFocalDistance();
-		double mx = cameraWidth / (2 * Math.tan(horizfov * Math.PI/360));
-		double ax = f*mx;
-		double camSep = 0.35;
-		
-		
-		Matrix Cpre = new Matrix(new double[][]{{ax,0,0,0}, {0,ax,0,0}, {0,0,1,0}});
-		
-		Matrix p1r = new Matrix(new double[]{5.8, 2.5, 1.9, 1}, 	4);
-		Matrix p2r = new Matrix(new double[]{3.2, 5.9, 4.9, 1}, 	4);
-		Matrix p3r = new Matrix(new double[]{5.3, 3.4, 3.4, 1}, 	4);
-		Matrix p4r = new Matrix(new double[]{2.7, 4.3, 1.4, 1}, 	4);
-		Matrix p5r = new Matrix(new double[]{4.2, 1.6, 4.6, 1}, 	4);
-		Matrix p6r = new Matrix(new double[]{1.9, 1, 1.9, 	1}	,	4);
-		ArrayList<Matrix> rightPoints = new ArrayList<>(6);
-		rightPoints.add(p1r);
-		rightPoints.add(p2r);
-		rightPoints.add(p3r);
-		rightPoints.add(p4r);
-		rightPoints.add(p5r);
-		rightPoints.add(p6r);
-		
+		double camSep = ap.getDrone().getCameraSeparation();
+		double tu = cameraWidth / 2;
+		double tv = cameraHeight / 2;
+		double TxLeft = camSep/2;
+		double TxRight = -camSep/2;
 
-		Matrix p1l = new Matrix(new double[]{5.8 + camSep, 2.5, 1.9, 1}, 	4);
-		Matrix p2l = new Matrix(new double[]{3.2 + camSep, 5.9, 4.9, 1}, 	4);
-		Matrix p3l = new Matrix(new double[]{5.3 + camSep, 3.4, 3.4, 1}, 	4);
-		Matrix p4l = new Matrix(new double[]{2.7 + camSep, 4.3, 1.4, 1}, 	4);
-		Matrix p5l = new Matrix(new double[]{4.2 + camSep, 1.6, 4.6, 1}, 	4);
-		Matrix p6l = new Matrix(new double[]{1.9 + camSep, 1, 1.9, 	1},		4);
-		ArrayList<Matrix> leftPoints = new ArrayList<>(6);
-		leftPoints.add(p1l);
-		leftPoints.add(p2l);
-		leftPoints.add(p3l);
-		leftPoints.add(p4l);
-		leftPoints.add(p5l);
-		leftPoints.add(p6l);
+		Matrix p1 = new Matrix(new double[] { 5.8, 2.5, -1.9, 1 }, 4);
+		Matrix p2 = new Matrix(new double[] { -3.2, 5.9, 4.9, 1 }, 4);
+		Matrix p3 = new Matrix(new double[] { -5.3, -3.4, -3.4, 1 }, 4);
+		Matrix p4 = new Matrix(new double[] { 2.7, -4.3, 1.4, 1 }, 4);
+		Matrix p5 = new Matrix(new double[] { 4.2, 1.6, 4.6, 1 }, 4);
+		Matrix p6 = new Matrix(new double[] { 1.9, -1, 1.9, 1 }, 4);
 
-//		5.8 5.5 1.9 
-//		3.2 5.9 4.9 
-//		5.3 5.4 3.4 
-//		2.7 4.3 1.4 
-//		4.2 1.6 4.6 
-//		5.9 1 	1.9
-		
-		
+//		Matrix KLeft = new Matrix(new double[][] { 	{ f, 0, tu, TzLeft * tu }, 
+//													{ 0, -f, tv, TzLeft * tv }, 
+//													{ 0, 0, 1, TzLeft } });
+//		Matrix KRight = new Matrix(new double[][] { { f, 0, tu, TzRight*tu }, { 0, -f, tv, TzRight*tv }, { 0, 0, 1, TzRight } });
+
+		Matrix KLeft = new Matrix(new double[][] { 	
+			{ -f, 0, tu, TxLeft * -f }, 
+			{ 0, f, tv, TxLeft * f }, 
+			{ 0, 0, 1, 0 } });
+		Matrix KRight = new Matrix(new double[][] { 
+			{ f, 0, tu, TxRight*f }, 
+			{ 0, -f, tv, TxRight*(-f) }, 
+			{ 0, 0, 1, 0 } });
+
+		ArrayList<Matrix> points = new ArrayList<>(6);
+		points.add(p1);
+		points.add(p2);
+		points.add(p3);
+		points.add(p4);
+		points.add(p5);
+		points.add(p6);
+
 		double[][] coeffL = new double[12][12];
 		double[][] coeffR = new double[12][12];
-		
-		for(int i = 0; i < rightPoints.size(); i++) {
-			Matrix uvw = Cpre.times(rightPoints.get(i));
 
-			double u_accent = (uvw.get(0, 0)/uvw.get(2, 0));
-			double v_accent =  (uvw.get(1, 0)/uvw.get(2, 0));
+		for (int i = 0; i < points.size(); i++) {
+			globalpoint = points.get(i).getArray();
+			double x = points.get(i).getArray()[0][0];
+			double y = points.get(i).getArray()[1][0];
+			double z = points.get(i).getArray()[2][0];
+			System.out.println("www");
+			System.out.println(points.get(i).getArray()[3][0]);
+			double w = points.get(i).getArray()[3][0]; // for clarity of formulas
 
+			Matrix uvwL = KLeft.times(points.get(i));
+			int u = (int) (uvwL.getArray()[0][0] / uvwL.getArray()[2][0]);
+			int v = (int) (uvwL.getArray()[1][0] / uvwL.getArray()[2][0]);
+
+			coeffL[2 * i] = new double[] { -x, 0, u * x, -y, 0, u * y, -z, 0, u * z, -w, 0, u * w };
+			coeffL[2 * i + 1] = new double[] { 0, -x, v * x, 0, -y, v * y, 0, -z, v * z, 0, -w, v * w };
 			
-			//add coefficients to coefficients matrix
-			coeffR[2 * i] = new double[] { -rightPoints.get(i).get(0, 0), 0, u_accent*rightPoints.get(i).get(0, 0),-rightPoints.get(i).get(1, 0), 0, u_accent * rightPoints.get(i).get(1, 0), -rightPoints.get(i).get(2, 0), 0, u_accent*rightPoints.get(i).get(2, 0), -rightPoints.get(i).get(3, 0), 0, u_accent*rightPoints.get(i).get(3, 0) };
-			coeffR[2 * i + 1] = new double[] { 0, -rightPoints.get(i).get(0, 0), v_accent*rightPoints.get(i).get(0, 0), 0, -rightPoints.get(i).get(1, 0), v_accent*rightPoints.get(i).get(1, 0), 0, -rightPoints.get(i).get(2, 0), v_accent*rightPoints.get(i).get(2, 0), 0, -rightPoints.get(i).get(3, 0), v_accent*rightPoints.get(i).get(3, 0) };
 			
-			coeffL[2 * i] = new double[] { -leftPoints.get(i).get(0, 0), 0, u_accent*leftPoints.get(i).get(0, 0), -leftPoints.get(i).get(1, 0), 0, u_accent * leftPoints.get(i).get(1, 0), -leftPoints.get(i).get(2, 0), 0, u_accent*leftPoints.get(i).get(2, 0), -leftPoints.get(i).get(3, 0), 0, u_accent*leftPoints.get(i).get(3, 0) };
-			coeffL[2 * i + 1] = new double[] { 0, -leftPoints.get(i).get(0, 0), v_accent*leftPoints.get(i).get(0, 0), 0, -leftPoints.get(i).get(1, 0), v_accent*leftPoints.get(i).get(1, 0), 0, -leftPoints.get(i).get(2, 0), v_accent*leftPoints.get(i).get(2, 0), 0, -leftPoints.get(i).get(3, 0), v_accent*leftPoints.get(i).get(3, 0) };
+			uglobalL = u;
+			vglobalL = v;
+			// right
+
+			// 
+			
+			Matrix uvwR = KRight.times(points.get(i));
+			u = (int) (uvwR.getArray()[0][0] / uvwR.getArray()[2][0]);
+			v = (int) (uvwR.getArray()[1][0] / uvwR.getArray()[2][0]);
+			uglobalR = u;
+			vglobalR = v;
+
+			coeffR[2 * i] = new double[] { -x, 0, u * x, -y, 0, u * y, -z, 0, u * z, -w, 0, u * w };
+			coeffR[2 * i + 1] = new double[] { 0, -x, v * x, 0, -y, v * y, 0, -z, v * z, 0, -w, v * w };
+			
+	
 		}
-		coeffR[11] = new double[]{0,0,0,0,0,0,0,0,0,0,0,1};
-		coeffL[11] = new double[]{0,0,0,0,0,0,0,0,0,0,0,1};
-		double[] b = new double[]{0,0,0,0,0,0,0,0,0,0,0,1};
-		
-		Matrix zeros = new Matrix(b, 12);
-		Matrix coeffMatrixR = new Matrix(coeffR);
-		Matrix Cright = coeffMatrixR.solve(zeros);
-		Matrix coeffMatrixL = new Matrix(coeffL);
-		Matrix CLeft = coeffMatrixL.solve(zeros);
-		double[][] arr = CLeft.getArray();
-		
-		this.leftC = new Matrix(new double[][]{{arr[0][0], arr[3][0], arr[6][0], arr[9][0]}, {arr[1][0], arr[4][0], arr[7][0], arr[10][0]}, {arr[2][0], arr[5][0], arr[8][0], arr[11][0]}});
-		this.leftR1 = new Matrix(new double[]{arr[0][0], arr[3][0], arr[6][0], arr[9][0]}, 1);
-		this.leftR2 = new Matrix(new double[]{arr[1][0], arr[4][0], arr[7][0], arr[10][0]}, 1);
-		this.leftR3 = new Matrix(new double[]{arr[2][0], arr[5][0], arr[8][0], arr[11][0]}, 1);
 
-		arr = Cright.getArray();
-		this.rightC = new Matrix(new double[][]{{arr[0][0], arr[3][0], arr[6][0], arr[9][0]}, {arr[1][0], arr[4][0], arr[7][0], arr[10][0]}, {arr[2][0], arr[5][0], arr[8][0], arr[11][0]}});
-		
-		this.rightR1 = new Matrix(new double[]{arr[0][0], arr[3][0], arr[6][0], arr[9][0]}, 1);
-		this.rightR2 = new Matrix(new double[]{arr[1][0], arr[4][0], arr[7][0], arr[10][0]}, 1);
-		this.rightR3 = new Matrix(new double[]{arr[2][0], arr[5][0], arr[8][0], arr[11][0]}, 1);
-//		
+		coeffL[0] = new double[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		coeffR[0] = new double[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		Matrix zeroes = new Matrix(new double[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 12);
+
+		Matrix coeffLMatrix = new Matrix(coeffL);
+		Matrix coeffRMatrix = new Matrix(coeffR);
+//		coeffLMatrix.print(10, 10);
+//		coeffRMatrix.print(10, 10);
+//		zeroes.print(10, 10);
+
+		Matrix CvectorLeft = coeffLMatrix.solve(zeroes);
+		Matrix CvectorRight = coeffRMatrix.solve(zeroes);
+
+		leftR1 = CvectorLeft.getMatrix(0, 3, 0, 0).transpose();
+		leftR2 = CvectorLeft.getMatrix(4, 7, 0, 0).transpose();
+		leftR3 = CvectorLeft.getMatrix(8, 11, 0, 0).transpose();
+
+		rightR1 = CvectorRight.getMatrix(0, 3, 0, 0).transpose();
+		rightR2 = CvectorRight.getMatrix(4, 7, 0, 0).transpose();
+		rightR3 = CvectorRight.getMatrix(8, 11, 0, 0).transpose();
+
 	}
 	
+	int uglobalL;
+	int vglobalL;
+	int uglobalR;
+	int vglobalR;
+	
+	
+
 	public double[] calculateXYZ(int xLeft, int yLeft, int xRight, int yRight) {
-		//TODO x = u, y = v of omgekeerd?
 		int vLeft = yLeft;
 		int vRight = yRight;
 		int uLeft = xLeft;
@@ -127,17 +138,29 @@ public class Pinhole {
 		double[] firstR = (rightR3.times(uRight)).minus(rightR1).getArray()[0];
 		double[] secondL = (leftR3.times(vLeft)).minus(leftR2).getArray()[0];
 		double[] secondR = (rightR3.times(vRight)).minus(rightR2).getArray()[0];
-		
-		Matrix C = new Matrix(new double[][]{firstL, firstR, secondL, {0,0,0,1}});
-		C.print(10, 10);
-		Matrix b = new Matrix(new double[]{0,0,0,1}, 4);
+
+		Matrix C = new Matrix(new double[][] { secondR, firstR, secondL, {0,0,0,1} });
+		Matrix b = new Matrix(new double[] { 0, 0, 0, 1}, 4);
 		Matrix XYZ = C.solve(b);
-		//TODO delen door de 4e waarde van XYZ
-		System.out.println(Arrays.toString(new double[]{XYZ.getArray()[0][0], XYZ.getArray()[1][0], XYZ.getArray()[2][0]}));
-		return new double[]{XYZ.getArray()[0][0], XYZ.getArray()[1][0], XYZ.getArray()[2][0]};
+		return new double[] { 
+				XYZ.getArray()[0][0] / XYZ.getArray()[3][0],
+				XYZ.getArray()[1][0] / XYZ.getArray()[3][0], 
+				-XYZ.getArray()[2][0] / XYZ.getArray()[3][0] };
 
 	}
 	
-	
+	void testje() {
+		System.out.println("--------------");
+		System.out.println("Test--");
+		System.out.println("left:");
+		System.out.println(uglobalL);
+		System.out.println(vglobalL);
+		System.out.println("right:");
+		System.out.println(uglobalR);
+		System.out.println(vglobalR);
+		System.out.println(globalpoint[0][0] + ", " + globalpoint[1][0] + ", " + globalpoint[2][0]);
+		System.out.println(Arrays.toString(calculateXYZ(uglobalL, vglobalL, uglobalR, vglobalR)));
+
+	}
 
 }
